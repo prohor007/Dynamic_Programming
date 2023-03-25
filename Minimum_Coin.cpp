f464b0d201200b2b7ea15c7db59c93e9784ef804
Prohor Paul <prohorpaul121@gmail.com>
2023-03-25 18:42:36 +0600

    Create Minimum_Coin.cpp

---
+++ Minimum_Coin.cpp
@@ -0,0 +1,62 @@
+//minimum coin need to make x . (coin supply infinity)
+
+#include<bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define all(v) v.begin(),v.end()
+#define INF (1 << 29)
+
+vector<ll> coin;
+ll dp[1005];
+
+ll minc(ll n){
+    if(n==0) return 0;
+    if(n<0) return INF;
+
+    if(dp[n] != -1) return dp[n];
+
+    ll res = INF;
+    for(ll c:coin){
+        res = min(res, 1+minc(n-c));
+    }
+
+    return dp[n] = res;
+}
+
+void solve(ll cs) {
+    ll n;
+    cout << "Number of Coin : " << endl;
+    cin >> n;
+
+    cout << "Enter Coins : " << endl;
+    for(ll i=0; i<n; i++){
+        ll y;
+        cin >> y;
+        coin.push_back(y);
+    }
+
+    ll x;
+    cout << "Enter Ammount : " << endl;
+    cin >> x;
+
+    ll result = minc(x);
+    if(result >= INF) cout << "IMPOSSIBLE" << endl;
+    else cout << "Minimum Number of Coins : " << result << endl;
+}
+
+int main() {
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(0), cout.tie(0);
+
+    memset(dp, -1, sizeof(dp));
+
+    ll t=1, cs=1;
+    //cin>>t;
+    while(t--) {
+        solve(cs++);
+    }
+
+    return 0;
+}
